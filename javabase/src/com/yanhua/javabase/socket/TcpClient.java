1bea56d5725e18b39996b12644f6166bcc9a53f5
xuyh <xuyh@missfresh.cn>
2020-02-02 12:26:05 +0800

    socket

---
+++ javabase/src/com/yanhua/javabase/socket/TcpClient.java
@@ -0,0 +1,23 @@
+package com.yanhua.javabase.socket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class TcpClient {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        //1.创建socket连接，指定ip和端口
+        Socket client = new Socket("127.0.0.1",10086);
+        OutputStream outputStream = client.getOutputStream();
+        outputStream.write("hello server".getBytes());
+        outputStream.flush();
+        InputStream inputStream = client.getInputStream();
+        byte[] buf = new byte[1024];
+        int read = inputStream.read(buf);
+        System.out.println("打印来自服务端的数据：" + new String(buf, 0, read));
+        outputStream.close();
+        inputStream.close();
+        client.close();
+    }
+}
---
+++ javabase/src/com/yanhua/javabase/socket/TcpServer.java
@@ -0,0 +1,44 @@
+package com.yanhua.javabase.socket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TcpServer {
+    public static void main(String[] args) throws IOException {
+        //1.创建socket，并绑定到特定的端口
+        ServerSocket serverSocket = new ServerSocket(10086);
+        while (true) {
+            //2.准备接收客户端连接
+            Socket client = serverSocket.accept();
+            new Thread(() -> {
+                InputStream inputStream = null;
+                OutputStream outputStream = null;
+                try {
+                    //3.读取客户端的数据
+                    inputStream = client.getInputStream();
+                    byte[] buf = new byte[1024];
+                    int read = inputStream.read(buf);
+                    System.out.println("打印来自客户端的数据：" + new String(buf, 0, read));
+                    //4.向客户端写入数据
+                    outputStream = client.getOutputStream();
+                    outputStream.write("hello client".getBytes());
+                    outputStream.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        outputStream.close();
+                        inputStream.close();
+                        client.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+        }
+    }
+}
