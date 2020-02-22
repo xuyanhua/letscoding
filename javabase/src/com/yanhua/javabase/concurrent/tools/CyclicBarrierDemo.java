ef22fd624d261d05ff29100ba22be82ca506ee77
xuyh <xuyh@missfresh.cn>
2020-02-23 01:06:51 +0800

    CyclicBarrierDemo

---
+++ javabase/src/com/yanhua/javabase/concurrent/tools/CyclicBarrierDemo.java
@@ -0,0 +1,38 @@
+package com.yanhua.javabase.concurrent.tools;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrierDemo {
+    public static void main(String[] args) {
+        new CyclicBarrierDemo().go();
+    }
+
+    public void go() {
+        CyclicBarrier barrier = new CyclicBarrier(3);
+        new Thread(new Task(barrier),"Thread1").start();
+        new Thread(new Task(barrier),"Thread2").start();
+        new Thread(new Task(barrier),"Thread3").start();
+    }
+
+    class Task implements Runnable {
+        private CyclicBarrier barrier;
+
+        public Task(CyclicBarrier barrier) {
+            this.barrier = barrier;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("线程" + Thread.currentThread().getName() + "已经到达" + System.currentTimeMillis());
+            try {
+                barrier.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+            System.out.println("线程" + Thread.currentThread().getName() + "开始处理" + System.currentTimeMillis());
+        }
+    }
+}
