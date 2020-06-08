b7fb4164deeb936065fac36fa677b4c5ce4c2ff6
xuyh <xuyh@missfresh.cn>
2020-06-08 11:03:12 +0800

    nio

--- javabase/src/com/yanhua/javabase/net/nio/NIOServer.java
+++ javabase/src/com/yanhua/javabase/net/nio/NIOServer.java
@@ -11,41 +11,63 @@ public class NIOServer {
     static int PORT = 10086;
     static int TIME_OUT = 3000;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        ServerSocketChannel serverChannel = null;//服务端通道
+        Selector selector = null;//选择器
+        try {
+            serverChannel = ServerSocketChannel.open();//打开通道
+            serverChannel.socket().bind(new InetSocketAddress(PORT));
+            serverChannel.configureBlocking(false);
 
-        ServerSocketChannel serverChannel = ServerSocketChannel.open();
-        serverChannel.socket().bind(new InetSocketAddress(PORT));
-        serverChannel.configureBlocking(false);
+            selector = Selector.open();//打开选择器
+            serverChannel.register(selector, SelectionKey.OP_ACCEPT);//要将通道注册到选择器的指定事件上
 
-        Selector selector = Selector.open();
-        serverChannel.register(selector, SelectionKey.OP_ACCEPT);
-
-        while (true) {
-            if (selector.select(TIME_OUT) == 0) {
-                System.out.println("等待接收...");
-                continue;
-            }
-            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
-            while (iterator.hasNext()) {
-                SelectionKey key = iterator.next();
-                if (key.isAcceptable()) {
-                    handleAccept(key);
-                } else if (key.isReadable()) {
-                    handleRead(key);
-                } else if (key.isWritable() && key.isValid()) {
-                    handleWrite(key);
-                } else if (key.isConnectable()) {
-                    System.out.println("isConnectable = true");
+            while (true) {
+                if (selector.select(TIME_OUT) == 0) {
+                    System.out.println("等待接收...");
+                    continue;
                 }
-                iterator.remove();
+                Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                while (iterator.hasNext()) {
+                    SelectionKey key = iterator.next();
+                    if (key.isAcceptable()) {//连接事件就绪
+                        handleAccept(key);
+                    } else if (key.isReadable()) {//读取事件就绪
+                        handleRead(key);
+                    } else if (key.isWritable() && key.isValid()) {//写入事件就绪
+                        handleWrite(key);
+                    } else if (key.isConnectable()) {//连接事件就绪
+                        System.out.println("isConnectable = true");
+                    }
+                    iterator.remove();
+                }
             }
+        } catch (Exception e) {
+
+        } finally {
+            if (serverChannel != null) {
+                try {
+                    serverChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (selector != null) {
+                try {
+                    selector.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
         }
+
     }
 
     private static void handleAccept(SelectionKey key) throws IOException {
         System.out.println("收到连接...");
         ServerSocketChannel severChannel = (ServerSocketChannel) key.channel();
-        SocketChannel channel = severChannel.accept();
+        SocketChannel channel = severChannel.accept();//监听连接
         channel.configureBlocking(false);
         channel.register(key.selector(), SelectionKey.OP_READ, ByteBuffer.allocate(BUF_SIZE));
 
