2ec69b7e817cbbbd87147161ed3c16df49dc213c
xuyh <xuyh@missfresh.cn>
2020-06-22 11:18:40 +0800

    二分法的优化，不用二分

--- algorithms/src/main/java/com/yanhua/algorithms/search/BinarySearch.java
+++ algorithms/src/main/java/com/yanhua/algorithms/search/BinarySearch.java
@@ -7,22 +7,35 @@ import java.util.Arrays;
  */
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = new int[]{10, 21, 23, 34, 51, 60, 67, 88, 89};
-        int key = 21;
+        int[] arr = new int[]{2, 4, 6, 8, 10, 12, 14, 16, 18};
+        int key = 20;
         int index = search(arr, key);
         System.out.printf("find %d from %s ,index=%d\n", key, Arrays.toString(arr), index);
+        int index2 = search2(arr, key);
+        System.out.printf("find %d from %s ,index=%d\n", key, Arrays.toString(arr), index2);
 
     }
 
+
+    /**
+     * 折半查找，时间复杂度O(logn)
+     *
+     * @param arr
+     * @param key
+     * @return
+     */
     public static int search(int[] arr, int key) {
         if (arr == null || arr.length == 0) {
             return -1;
         }
         int low = 0;
         int high = arr.length - 1;
+        int count = 0;
         do {
             int middle = (high + low) / 2;
+            count++;
             if (arr[middle] == key) {
+                System.out.println("比较次数：" + count);
                 return middle;
             } else if (arr[middle] > key) {
                 high = middle - 1;
@@ -30,6 +43,50 @@ public class BinarySearch {
                 low = middle + 1;
             }
         } while (low <= high);
+        System.out.println("比较次数：" + count);
         return -1;
     }
+
+
+    /**
+     * key-arr[low]
+     * 折半查找的改进，将1/2改成   ----------------- * (high-low)
+     * arr[high]-arr[low]
+     * 这种方式，越是平均分布，速度越快，如果完全平均，仅需一次查找
+     *
+     * @param arr
+     * @param key
+     * @return
+     */
+    public static int search2(int[] arr, int key) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        int low = 0;
+        int high = arr.length - 1;
+        int count = 0;
+        do {
+            int middle = low + (high - low) * (key - arr[low]) / (arr[high] - arr[low]);
+            if (middle > high) {
+                if (arr[high] == key) {
+                    return high;
+                } else {
+                    return -1;
+                }
+            }
+            count++;
+            if (arr[middle] == key) {
+                System.out.println("比较次数：" + count);
+                return middle;
+            } else if (arr[middle] > key) {
+                high = middle - 1;
+            } else {
+                low = middle + 1;
+            }
+        } while (low <= high);
+        System.out.println("比较次数：" + count);
+        return -1;
+    }
+
+
 }
